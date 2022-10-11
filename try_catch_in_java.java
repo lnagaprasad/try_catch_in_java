29d502eecf8dc26771b492136ec80245e100a138
pavithralagisetty <115474342+pavithralagisetty@users.noreply.github.com>
2022-10-11 23:46:17 +0530

    Update try_catch_in_java.java

--- try_catch_in_java.java
+++ try_catch_in_java.java
@@ -1,5 +1,3 @@
-#Please suggest more catch blocks wherever required
-#some times its getting exceptions,run time errors, sugges try catch blocks wherever required
 import java.util.Scanner;
 class exception 
 {
@@ -10,7 +8,14 @@ class exception
     System.out.println("Input two integers");
     a = input.nextInt( );
     b = input.nextInt( );
-    result = a / b;
-    System.out.println("Result = " + result);
+    try 
+    {
+      result = a / b;
+      System.out.println("Result = " + result);
+    }
+    catch (ArithmeticException e) 
+    {
+      System.out.println("Exception caught: Division by zero.");
+     }
   }
 }
